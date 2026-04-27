bacc86f762ad6fcf9eb687e245f270bb523ac881
IsaacVRey <isaac.vega.rey@gmail.com>
2026-04-27 02:14:13 -0600

    Calendario

--- Huella/Frontend/js/feed.js
+++ Huella/Frontend/js/feed.js
@@ -438,4 +438,12 @@ document.addEventListener('DOMContentLoaded', () => {
             updateFilterCoords(userLat, userLng);
         });
     }
+
+    document.getElementById('filter-fecha-inicio').addEventListener('click', function () {
+        this.showPicker();
+    });
+
+    document.getElementById('filter-fecha-fin').addEventListener('click', function () {
+        this.showPicker();
+    });
 });
